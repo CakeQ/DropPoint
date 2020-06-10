50f8eaa1a23ecfa32c09da6b1d01f81cde16c74a
Daniel Thompson <d.kqgqn@gmail.com>
2020-06-10 22:48:58 +0100

    Makes classes blueprint accessible

---
+++ Source/DropPoint/DropPointUI.h
@@ -0,0 +1,21 @@
+// Copyright Daniel Thompson @ https://github.com/CakeQ and Archie Whitehead 2020 All Rights Reserved.
+
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Blueprint/UserWidget.h"
+//#include "DropPointUI.generated.h"
+//
+///**
+// * 
+// */
+//UCLASS()
+//class DROPPOINT_API UDropPointUI : public UUserWidget
+//{
+//	GENERATED_BODY()
+//
+//public:
+//	UDropPointUI(const FObjectInitializer& ObjectInitializer);
+//
+//	virtual void NativeConstruct() override;
+//};
